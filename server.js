6315f5e258eabb3fc6d695b3809dc44ec379a4d5
PapaSshark <dylanforza0@gmail.com>
2026-09-23 19:40:14 +0200

    Add CORS middleware to server

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 
 const express = require("express");
 
+const cors = require("cors");
 const app = express();
-app.use(express.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 10000;
 const API_KEY = process.env.TWELVE_DATA_API_KEY || "";
